c9180bbe04c24157bb5b3c50edee519ccd1485c2
JosueMonterrey <josuemr0206@gmail.com>
2025-09-30 07:33:06 -0600

    Implementar la clase Nodo en L30.cpp

--- L30.cpp
+++ L30.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class Nodo {
+    private:
+    char letra;
+    int vecesRepetido;
+    vector<Nodo *> hijos;
+
+    public:
+    Nodo(char l) : letra(l) {}
+
+    char getLetra() { return letra; }
+
+    int getVecesRepetido() { return vecesRepetido; }
+    void incVecesRepetido() { vecesRepetido++; }
+
+    Nodo * getHijo(char l) {
+        for (Nodo * h : hijos) {
+            if (h->letra == l) return h;
+        }
+        return nullptr;
+    }
+    void agregarHijo(Nodo * nuevoHijo) {
+        hijos.push_back(nuevoHijo);
+    }
+};
+
 int main() {
-    
+
 }
