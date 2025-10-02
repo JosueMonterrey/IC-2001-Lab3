436e749bd339813c3138aab2ea2fc353b4f689ef
JosueMonterrey <josuemr0206@gmail.com>
2025-10-02 17:12:31 -0600

    Modificar L31.cpp para que los nodos utilicen un mapa en vez de un vector para mantener referencia de sus hijos.

--- L31.cpp
+++ L31.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 class Nodo {
     private:
-    vector<Nodo *> hijos;
+    map<char, Nodo *> hijos;
     
     public:
-    Nodo() : letra(' ') { }                     // usado para la raiz
-    Nodo(char chr) : letra(chr) { }
+    Nodo() : letra(' ') { }
     
     char letra;
 
-    Nodo * getHijo(char l) {
-        for (Nodo * h : hijos) {
-            if (h->letra == l) return h;
-        }
-        return nullptr;
+    Nodo * getHijo(char letra) {
+        if (!hijos.count(letra)) return nullptr;
+        return hijos[letra];
     }
 
-    void agregarHijo(Nodo * nuevoHijo) {
-        hijos.push_back(nuevoHijo);
+    Nodo * agregarHijo(char letra) {
+        Nodo * newHijo = new Nodo();
+        hijos.insert(pair<char, Nodo *>(letra, newHijo));
+        return newHijo;
     }
 };
 
@@ -37,8 +37,7 @@ class Trie{
         
         // if esa letra no existe entre los hijos del nodoActual hay que agregarla.
         if (nodoLetra == nullptr) {
-            nodoLetra = new Nodo(letra);
-            nodoActual->agregarHijo(nodoLetra);
+            nodoLetra = nodoActual->agregarHijo(letra);
             cantNodos++;
         }
         
