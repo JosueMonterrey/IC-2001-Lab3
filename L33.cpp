2ba2b66c9bdb2cb4b35996a14c0059a54e3cc149
JosueMonterrey <josuemr0206@gmail.com>
2025-10-01 19:56:35 -0600

    Implementar funcion para imprimir todas las palabras del trie. Agregar una funcion de quicksort para elejir las palabras que minimicen operaciones de borrado. Actualizar funcion agregarLetra() para que cada nodo tenga una cuenta del tamaño del subarbol

--- L33.cpp
+++ L33.cpp
@@ -4,14 +4,15 @@
 using namespace std;
 
 class Nodo {
-    private:
-    vector<Nodo *> hijos;
-    
     public:
     Nodo() : letra(' ') { }                     // usado para la raiz
-    Nodo(char chr) : letra(chr) { }
+    Nodo(char chr, bool esUltimaLetra) : letra(chr), formaPalabra(esUltimaLetra) { }
     
     char letra;
+    bool formaPalabra = false;
+    int sizeSubarbol = 1;
+
+    vector<Nodo *> hijos;
 
     Nodo * getHijo(char l) {
         for (Nodo * h : hijos) {
@@ -25,24 +26,95 @@ class Nodo {
     }
 };
 
+int dividirVector(vector<Nodo *> &vec, int low, int high) {
+    Nodo * pivote = vec[high];
+
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (vec[j]->sizeSubarbol <= pivote->sizeSubarbol) {
+            i++;
+            swap(vec[i], vec[j]);
+        }
+    }
+
+    swap(vec[i + 1], vec[high]);
+    return (i + 1);
+}
+
+void quickSort(vector<Nodo *> &vec, int low, int high) {
+    if (low < high) {
+        int pi = dividirVector(vec, low, high);
+
+        quickSort(vec, low, pi - 1);
+        quickSort(vec, pi + 1, high);
+    }
+}
+
 class Trie{
     private:
     Nodo * raiz;
 
-    void agregarLetra(Nodo * nodoActual, string palabra, int indiceLetra) {
+    int countPalabras = 0;
+
+    // Ocupamos imprimir primero la cantidad de operaciones y despues listar dichas operaciones.
+    // Entonces vamos haciendo las operaciones, guardandolas aqui, las contamos.
+    // Y al final, se imprime primero la cantidad y despues esta variable.
+    string printData;
+
+    int agregarLetra(Nodo * nodoActual, string palabra, int indiceLetra) {
         char letra = palabra[indiceLetra];
         bool esUltimaLetra = !palabra[indiceLetra + 1];             // los strings terminan con un character 'NUL'=00000000.
         Nodo * nodoLetra = nodoActual->getHijo(letra);
-        
+        int cantNuevosNodos = 0;
+
         // if esa letra no existe entre los hijos del nodoActual hay que agregarla.
         if (nodoLetra == nullptr) {
-            nodoLetra = new Nodo(letra);
+            nodoLetra = new Nodo(letra, esUltimaLetra);
             nodoActual->agregarHijo(nodoLetra);
+            cantNuevosNodos++;
         }
-        
-        if (!esUltimaLetra) {
-            agregarLetra(nodoLetra, palabra, indiceLetra + 1);  // seguir recorriendo el trie con la siguiente letra.
+
+        if (esUltimaLetra) {
+            countPalabras++;
         }
+        else {
+            cantNuevosNodos += agregarLetra(nodoLetra, palabra, indiceLetra + 1);  // seguir recorriendo el trie con la siguiente letra.
+        }
+
+        nodoActual->sizeSubarbol += cantNuevosNodos;
+        return cantNuevosNodos;
+    }
+
+    int imprimirTrieAux(Nodo * nodoActual) {
+        int cuentaOps = 0;
+
+        // es necesario recorrer el arbol empezando por las rutas con menos nodos para minimizar operaciones de borrado.
+        quickSort(nodoActual->hijos, 0, nodoActual->hijos.size() - 1);
+
+        for (Nodo * nodoLetra : nodoActual->hijos) {
+            // imprimir letra
+            printData += nodoLetra->letra;
+            printData += "\n";
+            cuentaOps++;
+
+            // indicar si termina la palabra
+            if (nodoLetra->formaPalabra) {
+                printData += "P\n";
+                cuentaOps++;
+                countPalabras--;        // lleva la cuenta de palabras que nos faltan para imprimir todas.
+            }
+
+            // imprimir recursivamente todas las letras hijas
+            cuentaOps += imprimirTrieAux(nodoLetra);
+
+            if (countPalabras == 0) break;  // si ya escribimos todas las palabras terminar el ciclo sin borrar la ultima palabra (no es necesario)
+
+            printData += "-\n";
+            cuentaOps++;
+        }
+
+        return cuentaOps;
     }
 
     public:
@@ -52,8 +124,24 @@ class Trie{
     void agregarPalabra(string palabra) {
         agregarLetra(raiz, palabra, 0);
     }
+
+    void imprimirTrie() {
+        int pasos = imprimirTrieAux(raiz);
+        cout << pasos << "\n" << printData;
+    }
 };
 
 int main() {
     Trie trie;
+
+    int N;
+    cin >> N;
+
+    while (N--) {
+        string palabra;
+        cin >> palabra;
+        trie.agregarPalabra(palabra);
+    }
+
+    trie.imprimirTrie();
 }
